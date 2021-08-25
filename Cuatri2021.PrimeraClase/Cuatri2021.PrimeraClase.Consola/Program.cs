9ceeb4c0db1c3c8b1af63314ab78189bbcdd4bdf
fran <francisco.vento@outlook.com>
2021-08-24 21:03:03 -0500

    primera clase

---
+++ Cuatri2021.PrimeraClase/Cuatri2021.PrimeraClase.Consola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cuatri2021.PrimeraClase.Validacion;
+
+namespace Cuatri2021.PrimeraClase.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero.");
+            string ingresoUsuario = Console.ReadLine();
+
+            int numeroIngresado = int.Parse(ingresoUsuario);
+
+            bool esValido = Validador.EsValido(numeroIngresado);
+
+            Console.WriteLine(esValido ? "Es válido" : "No es válido");
+
+            Console.WriteLine("Ingrese enter para salir");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Cuatri2021.PrimeraClase/Cuatri2021.PrimeraClase.Validacion/Validador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuatri2021.PrimeraClase.Validacion
+{
+    public class Validador
+    {
+
+        public static bool EsValido(int numero)
+        {
+            if(numero == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
