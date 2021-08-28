657e79f3e77dd807b0f20fc09c8dac91774475d7
fran <francisco.vento@outlook.com>
2021-08-28 18:56:10 -0500

    ejercicio 9

---
+++ Ejercicio9/Ejercicio9.console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese nombre de usuario: ");
+            string nombre = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(nombre))
+            {
+                Console.WriteLine("No te conozco");
+            } else
+            {
+                Console.WriteLine("Hola " + nombre + "!");
+            }
+
+            Console.WriteLine("Pulse una tecla para salir...");
+            Console.ReadLine();
+        }
+    }
+}
