3534576d748d96989ccb7752d75210d4dd24d6d8
fran <francisco.vento@outlook.com>
2021-08-30 03:19:33 -0500

    ejercicio 16

---
+++ Ejercicio16/Ejercicio16.console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string num;
+            bool flag;
+            do
+            {
+                Console.WriteLine("Ingrese un número");
+                num = Console.ReadLine();
+                flag = ValidarNum(num);
+            } while (!flag);
+
+            Console.WriteLine("El numero ingresado: {0} - Tiene {1} digitos. ", num, num.Length);
+            
+
+
+            Console.WriteLine("Pulse una tecla para salir");
+            Console.ReadLine();
+        }
+
+        static bool ValidarNum(string num)
+        {
+            if (Int32.TryParse(num, out int Salida))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
